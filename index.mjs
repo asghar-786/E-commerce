c5e4c4dbe81ede65f390fb8d66f527cfc6d2f16c
Asghar Ali <fa21bscs0017@maju.edu.pk>
2024-07-05 21:01:18 +0500

    vercel

--- index.mjs
+++ index.mjs
@@ -1,17 +1,3 @@
-// server.js or index.js (entry point for your server)
+import startServer from './src/App.mjs'
 
-import express from 'express';
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get('/', async (request, response) => {
-    const result = {
-        code: 200,
-        status: "ok",
-        message: "Express Server Is Running"
-    };
-    response.send(result);
-});
-
-export default app; // Export your Express app instance
+startServer();
--- src/App.mjs
+++ src/App.mjs
@@ -21,4 +21,5 @@ const startServer=async()=>{
     }
 }
 
-startServer()
+
+export default startServer
